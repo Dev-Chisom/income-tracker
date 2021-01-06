4b89330c9aa7cbe59553014d2eccdc96fd7fd2c9
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2021-01-06 21:07:12 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header :totalIncome="state.totalIncome" />
+    <Form />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, computed } from 'vue';
+import Header from './components/Header.vue';
+import Form from './components/Form.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const state = reactive({
+      income: [
+        {
+          value: 400,
+        },
+        {
+          value: 500,
+        },
+      ],
+      totalIncome: computed(() => {
+        let temp = 0;
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++) {
+            temp += state.income[i].value;
+          }
+        }
+        return temp;
+      }),
+    });
+    return {
+      Header,
+      Form,
+      state,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+body {
+  background: #eee;
 }
 </style>
